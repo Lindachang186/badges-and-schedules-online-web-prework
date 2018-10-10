74a0aaf5a4941cb62f116d02a1a36a9c3b8ccf50
Linda Chang <lindachang186@gmail.com>
2018-10-10 18:20:48 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,9 +26,9 @@ end
 
 def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
-    puts badge[0]
+    puts badge
   end
   assign_rooms(attendees).each do |assignment|
-    puts assignment[0]
+    puts assignment
   end
 end
