e95eedb0e6eaca9a72c7af38dcf35c18d390708a
Linda Chang <lindachang186@gmail.com>
2018-10-10 17:09:55 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,5 +18,5 @@ def assign_rooms(attendees)
     room_assignments.push("Hello, #{name}! You'll be assigned to room #{i}!")
     i +=1
   end
-  print room_assignments
+  return room_assignments
 end
